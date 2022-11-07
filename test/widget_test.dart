25cf35801eb0b0981e32cdd3eb4cdc953e1c0482
Sofy Nabillah <sofynabillah368@gmail.com>
2022-11-08 03:52:49 +0700

    commit oop, class, inheritance

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,28 @@
+//oop
+//class
+class Kendaraan {
+  String? merk;
+  String? nama;
+  int? harga;
+
+  //construktor
+  Kendaraan({this.merk, this.nama, this.harga});
+
+  //method
+  maju(int kecepatan) {
+    print(kecepatan.toString() + 'KM/Jam');
+  }
+}
+
+//Inheritance atau Pewarisan
+class Sedan extends Kendaraan {
+  int? jumlahPintu;
+  int? kecepatanMaksimal;
+
+  Sedan({String? merk, this.jumlahPintu, this.kecepatanMaksimal})
+      : super(merk: merk);
+}
+
 void main() {
   //tipe data dan variabel
 
@@ -122,7 +147,6 @@ void main() {
   print(prodi.length);
 
   //Perulangan Looping
-  print('PERULANGAN LOOPING');
   //For +
   for (int no = 1; no <= 5; no++) {
     print(no);
@@ -148,6 +172,31 @@ void main() {
     print(no3);
     no3++;
   } while (no3 <= no4);
+
+  //Instansiasi
+  var k1 = Kendaraan(merk: 'Honda', nama: 'Civic', harga: 500);
+  var kecepatan = k1.maju(70);
+
+  k1.merk = 'Honda';
+  print(kecepatan);
+  print(k1.maju(70));
+  print(k1.merk);
+  print(k1.nama);
+  print(k1.harga);
+
+  var k2 = Kendaraan(merk: 'Mitsubishi', nama: 'ColtDiesel', harga: 700);
+  var kecepatan2 = k2.maju(40);
+  print(kecepatan2);
+  print(k2.maju(40));
+  print(k2.merk);
+  print(k2.nama);
+  print(k2.harga);
+
+  var sedan1 = Sedan(jumlahPintu: 4, kecepatanMaksimal: 140, merk: 'Honda');
+  print('---------');
+  print(sedan1.jumlahPintu);
+  print(sedan1.kecepatanMaksimal);
+  print(sedan1.merk);
 }
 
 //function
